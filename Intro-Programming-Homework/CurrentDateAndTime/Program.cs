a6dac85905ce4a68e97de55071406b7869d5b309
koravski <jomyno@jomynoMac.local>
2015-01-11 18:29:34 +0200

    Homework

---
+++ Intro-Programming-Homework/CurrentDateAndTime/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CurrentDateAndTime
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine (DateTime.Now);
+		}
+	}
+}
---
+++ Intro-Programming-Homework/PrintMyName/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrintMyName
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Kaloian Koravski");
+		}
+	}
+}
