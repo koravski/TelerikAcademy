c11166f3020f0d4f6dfe3fe17127d9afef4d435a
koravski <kaioian@yahoo.com>
2015-02-13 14:57:13 +0200

    homework 3 changed names

---
+++ C#1/03.Operators-and-Expressions/Problem01OddOrEvenIntegers/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Problem1OddOrEvenIntegers
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			Start:
+			Console.Write ("Is it odd ");
+			int number;
+			number = Int32.Parse(Console.ReadLine());
+
+			bool IsOdd = true; 
+
+
+
+			if (number % 2 != 0)
+			{
+				Console.WriteLine(IsOdd);
+			}
+			else
+			{
+				Console.WriteLine(!IsOdd);
+			}
+
+			goto Start;
+
+
+
+
+
+			/*Start:
+			Console.Write ("Write a number: ");
+			int number;
+			number = Int32.Parse(Console.ReadLine());
+
+
+
+			if (number % 2 != 0)
+			{
+				Console.WriteLine(number + " is odd");
+			}
+			else
+			{
+				Console.WriteLine(number + " is even");
+			}
+
+			goto Start;*/
+
+		}
+	}
+}
---
+++ C#1/03.Operators-and-Expressions/Problem06FourDigitNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class FourDigitNumber
+{
+	static void Main()
+	{
+		Console.Write("Enter four-digit number: ");
+		int number = int.Parse(Console.ReadLine());
+		while (number > 9999 || number < 1000) {
+			Console.WriteLine("The number must be exactly 4 digits and cannot start with 0. Please, try again!");
+			number = int.Parse(Console.ReadLine());
+		}
+
+		int num1 = number / 1000;
+		int num2 = (number / 100) % 10;
+		int num3 = (number / 10) % 10;
+		int num4 = number % 10;
+		int sum = num1 + num2 + num3 + num4;
+
+		Console.WriteLine("sum of digits: " + sum);
+		Console.WriteLine("reversed: {0}{1}{2}{3}", num4, num3, num2, num1);
+		Console.WriteLine("last digit in front: {0}{1}{2}{3}", num4, num1, num2, num3);
+		Console.WriteLine("second and third digits exchanged: {0}{1}{2}{3}", num1, num3, num2, num4);
+	}
+}
