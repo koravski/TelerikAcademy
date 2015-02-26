a5ae26e32cbfb1353f0213893c024542b8b0bfa4
koravski <kaioian@yahoo.com>
2015-02-26 11:05:29 +0200

    Homework 6.Strings

---
+++ C#2/06.StringsAndTextProcessing/Problem16DateDifference/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Problem 16. Date difference
+
+    Write a program that reads two dates in the format: day.month.year and calculates the number of days between them.
+
+Example:
+
+Enter the first date: 27.02.2006
+Enter the second date: 3.03.2006
+Distance: 4 days
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Problem16DateDifference
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter the first date in the format [dd.MM.yyyy]: ");
+            string startDate = Console.ReadLine();
+            Console.Write("Enter the second date in the format [dd.MM.yyyy]: ");
+            string endDate = Console.ReadLine();
+            DateTime start = DateTime.ParseExact(startDate, "d.M.yyyy", CultureInfo.InvariantCulture);
+            DateTime end = DateTime.ParseExact(endDate, "d.M.yyyy", CultureInfo.InvariantCulture);
+            Console.WriteLine("Distance between {0} and {1} -> {2} days\n", startDate, endDate, Math.Abs((end - start).TotalDays));
+                
+        }
+    }
+}
---
+++ C#2/06.StringsAndTextProcessing/Problem22WordsCount/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Problem 22. Words count
+
+    Write a program that reads a string from the console and lists all different words in the string along with information how many times each word is found.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem22WordsCount
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter a string: ");
+            string text = Console.ReadLine();
+
+            string[] words = text.Split(new char[] {' ', ',','!','.','?'}, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (var t in words)
+            {
+                if (!dict.ContainsKey(t))
+                {
+                    dict.Add(t, 1);
+                }
+                else
+                {
+                    dict[t]++;
+                }
+            }
+            Console.WriteLine("\nLetter occurs:\n{0}\n",
+            string.Join("\n", dict.Select(x => string.Format(@"'{0}' -> {1} time(s)", x.Key, x.Value)).ToArray()));
+
+        }
+    }
+}
